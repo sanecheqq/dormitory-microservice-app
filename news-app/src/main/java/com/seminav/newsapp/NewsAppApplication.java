3eb2c231b23f14af6f5540fefa428fd0e9e15b3c
sanchq2 <semin.alex222@yandex.ru>
2024-02-19 14:23:14 +0300

    news app init

---
+++ news-app/src/main/java/com/seminav/newsapp/NewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.seminav.newsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAppApplication.class, args);
+	}
+
+}
