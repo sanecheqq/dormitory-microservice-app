60eee4d1a890d0423f8202165f9905f6d058b406
sanecheqq <semin.alex222@yandex.ru>
2024-02-19 16:59:30 +0300

    init ping controller

---
+++ news-app/src/main/java/com/seminav/newsapp/controllers/PingController.java
@@ -0,0 +1,12 @@
+package com.seminav.newsapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
