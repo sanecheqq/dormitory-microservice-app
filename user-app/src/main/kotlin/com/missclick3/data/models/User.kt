0a0787456a6adb68cf11861f80184ab0324ad556
missclick3 <aandreev171202@gmail.com>
2024-03-01 15:56:49 +0300

    Fixed user entity

--- user-app/src/main/kotlin/com/missclick3/data/models/User.kt
+++ user-app/src/main/kotlin/com/missclick3/data/models/User.kt
@@ -8,24 +8,26 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
 @Serializable
 data class User(
     val id: String? = "",
+    val username: String,
     val name: String,
     val surname: String,
-    val patronymic: String,
+    val patronymic: String? = "",
     val email: String,
-    val phoneNumber: String,
-    val tgUsername: String,
+    val phoneNumber: String? = "",
+    val tgUsername: String? = "",
     val address: String,
     val password: String,
     val salt: String
 )
 
 object Users: UUIDTable("users") {
+    val username = varchar("username", 128)
     val name = varchar("name", 128)
     val surname = varchar("surname", 128)
-    val patronymic = varchar("patronymic", 128)
+    val patronymic = varchar("patronymic", 128).nullable()
     val email = varchar("email", 256).like("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$")
-    val phoneNumber = varchar("phoneNumber", 12).like("\"^\\\\+7\\\\d{10}\$\"")
-    val tgUsername = varchar("tgUsername", 128)
+    val phoneNumber = varchar("phoneNumber", 12).nullable().like("\"^\\\\+7\\\\d{10}\$\"")
+    val tgUsername = varchar("tgUsername", 128).nullable()
     val password = varchar("password", 128)
     val salt = varchar("salt", 128)
 }
