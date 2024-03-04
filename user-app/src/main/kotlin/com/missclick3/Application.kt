348c643a2f6b291140273b99855e50e831312e95
missclick3 <aandreev171202@gmail.com>
2024-03-04 09:00:58 +0300

    Updated security plugin

--- user-app/src/main/kotlin/com/missclick3/Application.kt
+++ user-app/src/main/kotlin/com/missclick3/Application.kt
@@ -1,10 +1,15 @@
 package com.missclick3
 
 import com.missclick3.data.dao.DatabaseSingleton
+import com.missclick3.data.dao.user.UserDaoImpl
+import com.missclick3.data.models.User
 import com.missclick3.plugins.*
+import com.missclick3.security.token.TokenConfig
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -12,8 +17,14 @@ fun main() {
 }
 
 fun Application.module() {
+    val tokenConfig = TokenConfig(
+        issuer = System.getenv("JWT_ISSUER"),
+        audience = System.getenv("JWT_AUDIENCE"),
+        expiresIn = 365L * 1000L * 60L * 60L * 24L,
+        secret = System.getenv("JWT_SECRET")
+    )
     DatabaseSingleton.init()
-    configureSecurity()
+    configureSecurity(tokenConfig)
     configureMonitoring()
     configureSerialization()
     configureRouting()
