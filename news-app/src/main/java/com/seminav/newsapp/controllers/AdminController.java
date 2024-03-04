b40039f2fa267646021ac9e95dff97beb7a50828
sanecheqq <semin.alex222@yandex.ru>
2024-03-04 12:18:47 +0300

    fix admin controller post endpoint

--- news-app/src/main/java/com/seminav/newsapp/controllers/AdminController.java
+++ news-app/src/main/java/com/seminav/newsapp/controllers/AdminController.java
@@ -3,12 +3,10 @@ package com.seminav.newsapp.controllers;
 import com.seminav.newsapp.messages.CreateNewsRequest;
 import com.seminav.newsapp.messages.dtos.NewsDto;
 import com.seminav.newsapp.services.NewsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
@@ -18,9 +16,9 @@ public class AdminController {
 
     @PostMapping
     public ResponseEntity<NewsDto> createNews(
-            @RequestBody CreateNewsRequest createNewsRequest
+            @ModelAttribute @Valid CreateNewsRequest createNewsRequest
     ) {
         return ResponseEntity.ok(newsService.createNews(createNewsRequest));
-
     }
+
 }
