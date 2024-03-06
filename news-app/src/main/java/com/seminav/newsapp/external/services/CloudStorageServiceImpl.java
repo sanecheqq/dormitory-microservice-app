f96a697dd8a8c6f4473bda53e139333c3620e171
sanecheqq <semin.alex222@yandex.ru>
2024-03-06 16:23:15 +0300

    realize deleteFiles request to storage api

--- news-app/src/main/java/com/seminav/newsapp/external/services/CloudStorageServiceImpl.java
+++ news-app/src/main/java/com/seminav/newsapp/external/services/CloudStorageServiceImpl.java
@@ -1,15 +1,15 @@
 package com.seminav.newsapp.external.services;
 
 import com.seminav.newsapp.exceptions.ConvertMultipartFileToByteArrayResourceException;
+import com.seminav.newsapp.exceptions.DeleteFilesFromStorageException;
 import com.seminav.newsapp.exceptions.FilesCanNotBeNullException;
 import com.seminav.newsapp.exceptions.UploadFilesException;
+import com.seminav.newsapp.external.messages.DeleteFilesRequest;
 import com.seminav.newsapp.external.messages.FileDto;
 import com.seminav.newsapp.external.messages.UploadFilesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -17,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.List;
 
@@ -66,6 +67,25 @@ public class CloudStorageServiceImpl extends DiscoveryClientService implements C
 
     @Override
     public void deleteFiles(List<String> ids) {
-
+        if (ids.isEmpty()) {
+            return;
+        }
+        var storageInstance = getAvaliableServiceInstance("storage-app");
+        String deleteFilesUrl;
+        try {
+            deleteFilesUrl = storageInstance.getUri().resolve("/storage-api").toURL().toExternalForm();
+        } catch (MalformedURLException e) {
+            throw new DeleteFilesFromStorageException("Deleting failed while converting URI to URL " + storageInstance.getUri());
+        }
+        HttpEntity<DeleteFilesRequest> request = new HttpEntity<>(new DeleteFilesRequest(ids));
+        ResponseEntity<Void> deleteResponseEntity = restTemplate.exchange(
+                deleteFilesUrl,
+                HttpMethod.DELETE,
+                request,
+                Void.class
+        );
+        if (deleteResponseEntity.getStatusCode().isError()) {
+            throw new DeleteFilesFromStorageException("Deleting failed with code " + deleteResponseEntity.getStatusCode());
+        }
     }
 }
