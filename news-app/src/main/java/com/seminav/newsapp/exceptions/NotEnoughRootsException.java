6146523a72cd59d196113a2a1bfbf78a93b0e5a0
sanecheqq <semin.alex222@yandex.ru>
2024-03-13 17:51:32 +0300

    add checking role in AdminController

---
+++ news-app/src/main/java/com/seminav/newsapp/exceptions/NotEnoughRootsException.java
@@ -0,0 +1,7 @@
+package com.seminav.newsapp.exceptions;
+
+public class NotEnoughRootsException extends RuntimeException {
+    public NotEnoughRootsException(String message) {
+        super(message);
+    }
+}
