e1fec1fa65b1f17ff267ba80081b362877002ea7
sanecheqq <semin.alex222@yandex.ru>
2024-03-31 19:20:10 +0300

    add main configs and properties of app

---
+++ market-app/src/main/java/com/seminav/marketapp/config/AppConfiguration.java
@@ -0,0 +1,26 @@
+package com.seminav.marketapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class AppConfiguration implements WebMvcConfigurer {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/news/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT");
+        registry.addMapping("/admin/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT");
+    }
+}
