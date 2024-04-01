bac768383c088a273d38dac872be91ffe9e6c90a
sanecheqq <semin.alex222@yandex.ru>
2024-04-01 22:02:56 +0300

    ProductsController: endpoint for creating product

--- market-app/src/main/java/com/seminav/marketapp/controllers/ProductsController.java
+++ market-app/src/main/java/com/seminav/marketapp/controllers/ProductsController.java
@@ -1,11 +1,28 @@
 package com.seminav.marketapp.controllers;
 
+import com.seminav.marketapp.external.services.ExternalUserService;
+import com.seminav.marketapp.messages.CreateProductRequest;
+import com.seminav.marketapp.messages.dtos.ProductDto;
+import com.seminav.marketapp.services.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductsController {
+    private final ProductService productService;
+    private final ExternalUserService externalUserService;
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @ModelAttribute @Valid CreateProductRequest createProductRequest,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        externalUserService.getUserRoleFromUserAndValidateJWT(authorizationHeader);
+        ProductDto product = productService.createProduct(createProductRequest);
+        return ResponseEntity.ok(product);
+    }
 }
