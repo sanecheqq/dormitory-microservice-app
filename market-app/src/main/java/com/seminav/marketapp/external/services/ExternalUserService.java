31b90089023fcf82810307899b9ae2a89f1e9eb4
sanecheqq <semin.alex222@yandex.ru>
2024-04-01 21:23:04 +0300

    add external services

---
+++ market-app/src/main/java/com/seminav/marketapp/external/services/ExternalUserService.java
@@ -0,0 +1,14 @@
+package com.seminav.marketapp.external.services;
+
+
+import java.util.List;
+
+public interface ExternalUserService {
+    String getUserRoleFromUserAndValidateJWT(String authHeader);
+
+    List<String> getUserSavedProducts(String authHeader);
+
+//    UserDto getUserDto(String authHeader);
+
+    void deleteSavedProductsFromFollowers(String newsId, String authHeader);
+}
---
+++ market-app/src/main/java/com/seminav/marketapp/external/services/ExternalUserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.seminav.marketapp.external.services;
+
+import com.seminav.marketapp.exceptions.DeleteSavedProductFromFollowersException;
+import com.seminav.marketapp.external.messages.DeleteProductFromFollowersRequest;
+import com.seminav.marketapp.util.HeaderRequestInterceptor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ExternalUserServiceImpl extends DiscoveryClientService implements ExternalUserService {
+    private final RestTemplate restTemplate;
+
+    public ExternalUserServiceImpl(RestTemplate restTemplate) {
+        super();
+        this.restTemplate = restTemplate;
+    }
+    @Override
+    public String getUserRoleFromUserAndValidateJWT(String authHeader) {
+        var storageInstance = getAvaliableServiceInstance("user-app");
+        URI userUri = storageInstance.getUri().resolve("/authenticate");
+        restTemplate.setInterceptors(buildAuthHeaderInterceptorList(authHeader));
+        return restTemplate.getForObject(userUri, String.class);
+    }
+
+    @Override
+    public void deleteSavedProductsFromFollowers(String productId, String authHeader) {
+        var storageInstance = getAvaliableServiceInstance("user-app");
+        URI deleteSavedNewsFromFollowersUri = storageInstance.getUri().resolve("/saved-products/followers"); // todo: check routing
+        restTemplate.setInterceptors(buildAuthHeaderInterceptorList(authHeader));
+        HttpEntity<DeleteProductFromFollowersRequest> request = new HttpEntity<>(new DeleteProductFromFollowersRequest(productId));
+        ResponseEntity<String> response = restTemplate.exchange(deleteSavedNewsFromFollowersUri, HttpMethod.DELETE, request, String.class);
+        if (response.getStatusCode().is4xxClientError()) {
+            throw new DeleteSavedProductFromFollowersException("Product " + productId + " was not deleted from followers");
+        } else {
+            System.out.println("Product " + productId + " was deleted!");
+        }
+    }
+
+    public List<String> getUserSavedProducts(String authHeader) {
+        var storageInstance = getAvaliableServiceInstance("user-app");
+        URI userUri = storageInstance.getUri().resolve("/saved-products"); // todo: check routing
+        restTemplate.setInterceptors(buildAuthHeaderInterceptorList(authHeader));
+        String[] savedNewsIds = restTemplate.getForObject(userUri, String[].class);
+        if (savedNewsIds == null || savedNewsIds.length == 0) {
+            return new ArrayList<>();
+        } else {
+            return List.of(savedNewsIds);
+
+        }
+    }
+
+//    public UserDto getUserDto(String authHeader) {
+//        var storageInstance = getAvaliableServiceInstance("user-app");
+//        URI userUri = storageInstance.getUri().resolve("/user");
+//        restTemplate.setInterceptors(buildAuthHeaderInterceptorList(authHeader));
+//        return restTemplate.getForObject(userUri, UserDto.class);
+//    }
+
+    private static List<ClientHttpRequestInterceptor> buildAuthHeaderInterceptorList(String authHeader) {
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", authHeader));
+        return interceptors;
+    }
+
+
+}
