9816d829b5072937f840fb7c1ad238f86364f3de
sanecheqq <semin.alex222@yandex.ru>
2024-04-01 20:11:15 +0300

    init ProductsController

---
+++ market-app/src/main/java/com/seminav/marketapp/controllers/ProductsController.java
@@ -0,0 +1,11 @@
+package com.seminav.marketapp.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductsController {
+}
